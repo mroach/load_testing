b053db3df9d0160a396bfc270d61bfc142e599d4
Michael Roach <michael.roach@xing.com>
2018-11-21 21:37:56 +0100

    tidy-up config

--- bench.rb
+++ bench.rb
@@ -4,43 +4,50 @@ require "net/http"
 
 SYSTEM_CPUS = `sysctl -n hw.ncpu`.to_i
 DEFAULT_WORKERS = [(SYSTEM_CPUS * 0.8).floor, 3].max
+HOST = "127.0.0.1"
 PORT = 7000
 
+WRK_CONNECTIONS = ENV.fetch("WRK_CONNECTIONS", "10").to_i
+WRK_DURATION = ENV.fetch("WRK_CONNECTIONS", "5s")
+
+puts "System has #{SYSTEM_CPUS} CPUs. Default workers: #{DEFAULT_WORKERS}"
+
+Test = Struct.new(:dir, :cmd, :env, :host, :port) do
+  def initialize(dir, cmd, env: {}, host: HOST, port: PORT)
+    super(dir, cmd, env, host, port)
+  end
+end
+
 tests = {
-  rails_w2: {
-    dir: "ruby/railshost",
-    env: {},
-    cmd: "bundle exec puma -w 2 -p #{PORT} -e production --preload"
-  },
-  rails_w4: {
-    dir: "ruby/railshost",
-    env: {},
-    cmd: "bundle exec puma -w 4 -p #{PORT} -e production --preload"
-  },
-  rails_w6: {
-    dir: "ruby/railshost",
-    env: {},
-    cmd: "bundle exec puma -w 6 -p #{PORT} -e production --preload"
-  },
-  hanami: {
-    dir: "ruby/hanamihost",
-    env: {},
-    cmd: "bundle exec puma -w #{DEFAULT_WORKERS} -p #{PORT} -e production --preload"
-  },
-  phoenix: {
-    dir: "elixir/phoenixhost",
-    env: {"MIX_ENV" => "prod", "PORT" => PORT.to_s},
-    cmd: "mix phx.server"
-  },
-  amber: {
-    dir: "crystal/amberhost",
-    env: {"AMBER_ENV" => "production", "PORT" => PORT.to_s},
-    cmd: "bin/amberhost_prod"
-  }
+  rails_w2: Test.new(
+    "ruby/railshost",
+    "bundle exec puma -w 2 -p #{PORT} -e production --preload"),
+
+  rails_w4: Test.new(
+    "ruby/railshost",
+    "bundle exec puma -w 4 -p #{PORT} -e production --preload"),
+
+  rails_w6: Test.new(
+    "ruby/railshost",
+    "bundle exec puma -w 6 -p #{PORT} -e production --preload"),
+
+  hanami: Test.new(
+    "ruby/hanamihost",
+    "bundle exec puma -w #{DEFAULT_WORKERS} -p #{PORT} -e production --preload"),
+
+  phoenix: Test.new(
+    "elixir/phoenixhost",
+    "mix phx.server",
+    env: {"MIX_ENV" => "prod", "PORT" => PORT.to_s}),
+
+  amber: Test.new(
+    "crystal/amberhost",
+    "bin/amberhost_prod",
+    env: {"AMBER_ENV" => "production", "PORT" => PORT.to_s})
 }
 
 def alive?
-  uri = URI("http://127.0.0.1:#{PORT}/warmup")
+  uri = URI("http://#{HOST}:#{PORT}/warmup")
   Net::HTTP.get_response(uri).code == "200"
 rescue Errno::ECONNREFUSED
   false
@@ -58,13 +65,12 @@ def started_after_waiting?(limit:)
 end
 
 def start_server(name, spec)
-  dir, env, cmd = spec[:dir], spec[:env], spec[:cmd]
-  env_desc = env.map { |k,v| "#{k}=#{v}" }.join(" ")
+  env_desc = spec.env.map { |k,v| "#{k}=#{v}" }.join(" ")
   log_file = File.open("results/log/#{name}_server_output.log", "w+")
 
-  puts "Starting #{name} server: #{dir}> #{env_desc} #{cmd}"
-  Dir.chdir(dir) do
-    return Process.spawn(env, cmd, out: log_file)
+  puts "Starting #{name} server: #{spec.dir}> #{env_desc} #{spec.cmd}"
+  Dir.chdir(spec.dir) do
+    return Process.spawn(spec.env, spec.cmd, out: log_file)
   end
 end
 
@@ -79,14 +85,14 @@ tests.each do |name, spec|
 
   puts "Server running at #{pid}"
 
-  platform = Net::HTTP.get(URI("http://127.0.0.1:#{PORT}/platform"))
+  platform = Net::HTTP.get(URI("http://#{HOST}:#{PORT}/platform"))
   puts "Platform is #{platform}"
 
   result_file = "results/#{name}_bench_results.json"
 
   Kernel.system(
     {"WRK_JSON_OUT" => result_file, "WRK_JSON_DESC" => platform},
-    "wrk -c10 -d3s -s bench.lua http://127.0.0.1:#{PORT}"
+    "wrk -c #{WRK_CONNECTIONS} -d #{WRK_DURATION} -s bench.lua http://#{HOST}:#{PORT}"
   )
 
   puts "Shutting down server #{pid}"
